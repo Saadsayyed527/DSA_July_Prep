712385cc0d41dab6ade5160e203577ccbde5e4cb
Saadsayyed527 <137900721+Saadsayyed527@users.noreply.github.com>
2025-02-26 12:40:20 +0530

    day 2

---
+++ day2.java
@@ -0,0 +1,37 @@
+public class day2 {
+    public static void main(String[] args) {
+        // 1) Find the Sum of All Odd Elements in an Array
+// int arr[] = {1,2,3,4,5};
+// int sum =0;
+// for(int i =0;i<arr.length;i++){
+//     if(arr[i]%2 != 0){
+//         sum = sum + arr[i];
+//     }
+// }
+
+// System.err.println(sum);
+
+        // 2) Find the Average of All Elements in an Array.
+
+//         int arr[] = {1,2,3,4,5};
+// int count =0;
+// int sum = 0;
+// for(int i =0;i<arr.length;i++){
+// sum = sum + arr[i];
+//        count ++ ;
+   
+// }
+// System.err.println(sum/count);
+
+
+        // 3 ) Print an Array in Reverse Order 
+
+      int arr[] = {1,2,3,4,5};
+
+for(int i = arr.length-1;i>=0;i--){
+    System.out.println(arr[i]);
+}
+
+
+    }
+}
